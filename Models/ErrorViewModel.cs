821ccf74b1d30c29a89d58314f5c7cbb0a56131e
SaiyedRushan <rushan52@gmail.com>
2024-05-05 23:11:32 -0400

    init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Auction.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
